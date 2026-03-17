9121dfc9c5868eb3489e76949f9927ae79a9e450
dimonalek <dimonalek@gmail.com>
2026-03-17 17:07:43 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -23,11 +23,3 @@ def long_time_func(first: int, second: int, third: int) -> int:
 @cache
 def long_time_func_2(n_tuple: tuple, power: int) -> int:
     return [number ** power for number in n_tuple]
-
-
-long_time_func(1, 2, 3)
-long_time_func(2, 2, 3)
-long_time_func_2((5, 6, 7), 5)
-long_time_func(1, 2, 3)
-long_time_func_2((5, 6, 7), 10)
-long_time_func_2((5, 6, 7), 10)
